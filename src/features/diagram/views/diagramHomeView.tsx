01c52d4385c40da5ccdc5a05cb9a540a873a59f7
roswell <roswell_pando@outlook.es>
2025-03-31 21:30:41 -0500

    rendering nodes with local data

--- src/features/diagram/views/diagramHomeView.tsx
+++ src/features/diagram/views/diagramHomeView.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-
+import DatabaseDiagram from "../components/DatabaseDiagram";
 interface Params {
 	diagramId: string;
 }
@@ -10,7 +10,11 @@ export default function DiagramHomeView() {
 	return (
 		<>
 			<header>Header diagram home view</header>
-			<main>Main diagram home view. Diagram id {diagramId}</main>
+			<main>Main diagram home view. Diagram id {diagramId}
+        <div style={{ width: "100%", height: "600px" }}>
+          <DatabaseDiagram />
+        </div>
+      </main>
 			<footer>Footer diagram home view</footer>
 		</>
 	);
