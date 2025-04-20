70539007cfec4ad061d13a6b9654b5d5be4b5f83
Anghelo Alagon <200518@unsaac.edu.pe>
2025-04-20 01:22:11 -0500

    set Sidebar to be collapsed by default with defaultOpen=false

--- src/features/modals/canva/components/Sidebar/LayoutDiagram.tsx
+++ src/features/modals/canva/components/Sidebar/LayoutDiagram.tsx
@@ -1,16 +1,11 @@
-import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/features/modals/canva/components/Sidebar/AppSidebar";
 
 export function LayoutDiagram({ children }: { children: React.ReactNode }) {
 	return (
-		<SidebarProvider>
+		<SidebarProvider defaultOpen={false}>
 			<AppSidebar />
 
-			<SidebarTrigger className="" />
 			<SidebarInset>
 				<main className="h-full w-full overflow-hidden">{children}</main>
 			</SidebarInset>
