ce4a3091ae08be81e1394234f0ee69846aa86273
Anghelo Alagon <200518@unsaac.edu.pe>
2025-04-19 21:59:27 -0500

    add types to sidebar and user data

---
+++ src/features/modals/canva/components/Sidebar/types.ts
@@ -0,0 +1,18 @@
+import type { ReactElement } from "react";
+
+export type NavItem = {
+	title: string;
+	icon: ReactElement;
+	isActive?: boolean;
+};
+
+export type User = {
+	name: string;
+	email: string;
+	avatar: string;
+};
+
+export type Data = {
+  navMain: NavItem[];
+	user: User;
+}
