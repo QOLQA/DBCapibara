5ed7ebe9dbe1de8fec6056b2d4887f888cac8809
Anghelo Alagon <200518@unsaac.edu.pe>
2025-05-07 21:06:01 -0500

    move layaout and add title as promp

--- src/features/modals/canva/LayoutDiagram.tsx
+++ src/features/modals/canva/LayoutDiagram.tsx
@@ -1,12 +1,13 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/features/modals/canva/components/Sidebar/AppSidebar";
-import { AppHeader } from "../header/AppHeader";
+import { AppHeader } from "./components/header/AppHeader";
+
 
 export function LayoutDiagram({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<div className="flex flex-col h-screen w-screen z-50 overflow-hidden">
-				<AppHeader />
+				<AppHeader title="" />
 
 				<SidebarProvider
 					className="overflow-hidden h-full w-full"
