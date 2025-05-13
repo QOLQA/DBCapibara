fae6065a23a246211f634e327ab0602475b98161
STYersonH <yerson2737182731@gmail.com>
2025-05-13 06:24:35 -0500

    feat: delete table and atribute (#24)

--- src/features/modals/canva/components/Sidebar/AppSidebar.tsx
+++ src/features/modals/canva/components/Sidebar/AppSidebar.tsx
@@ -1,52 +1,52 @@
 import { Sidebar, useSidebar } from "@/components/ui/sidebar";
 import {
-	Calendar,
-	Database,
-	DataPie,
+  Calendar,
+  Database,
+  DataPie,
 } from "../../../../../components/icons/sidebar";
 import { SidebarIcons } from "./SidebarIcons";
 import { useEffect, useState } from "react";
 import { SidebarContentPrinc } from "./SidebarContent";
 
 const data = {
-	user: {
-		name: "shadcn",
-		email: "m@example.com",
-		avatar: "/user.png",
-	},
-	navMain: [
-		{
-			title: "Database",
-			icon: <Database />,
-			isActive: true,
-		},
-		{
-			title: "Consultas",
-			icon: <Calendar />,
-		},
-		{
-			title: "Estadísticas",
-			icon: <DataPie />,
-		},
-	],
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/user.png",
+  },
+  navMain: [
+    {
+      title: "Database",
+      icon: <Database />,
+      isActive: true,
+    },
+    {
+      title: "Consultas",
+      icon: <Calendar />,
+    },
+    {
+      title: "Estadísticas",
+      icon: <DataPie />,
+    },
+  ],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	const [activeItem, setActiveItem] = useState(data.navMain[0]);
-	const { setOpen } = useSidebar();
-	return (
-		<Sidebar
-			collapsible="icon"
-			className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row [&>[data-sidebar=sidebar]]:bg-secondary-gray py-5 bg-secondary-gray"
-			{...props}
-		>
-			<SidebarIcons
-				data={data}
-				activeItem={activeItem}
-				setActiveItem={setActiveItem}
-				setOpen={setOpen}
-			/>
-			<SidebarContentPrinc activeItem={activeItem} data={data} />
-		</Sidebar>
-	);
+  const [activeItem, setActiveItem] = useState(data.navMain[0]);
+  const { setOpen } = useSidebar();
+  return (
+    <Sidebar
+      collapsible="icon"
+      className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row [&>[data-sidebar=sidebar]]:bg-secondary-gray py-5 bg-secondary-gray"
+      {...props}
+    >
+      <SidebarIcons
+        data={data}
+        activeItem={activeItem}
+        setActiveItem={setActiveItem}
+        setOpen={setOpen}
+      />
+      <SidebarContentPrinc activeItem={activeItem} data={data} />
+    </Sidebar>
+  );
 }
