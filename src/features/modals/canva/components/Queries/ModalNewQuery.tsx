5581269ced43860c2348b5e6b08e18dffa140038
Anghelo Alagon <200518@unsaac.edu.pe>
2025-05-18 12:22:24 -0500

    create tsx to query section's modal

---
+++ src/features/modals/canva/components/Queries/ModalNewQuery.tsx
@@ -0,0 +1,5 @@
+export const ModalNewQuery = () => {
+  return (
+    <div>ModalNewQuery</div>
+  )
+}
---
+++ src/features/modals/canva/components/Queries/ModalSelectDocs.tsx
@@ -0,0 +1,5 @@
+export const ModalSelectDocs = () => {
+  return (
+    <div>ModalSelectDocs</div>
+  )
+}
