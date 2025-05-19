ce3a3ab8e05fea356dfa68b36054e2f98e03efc6
Anghelo Alagon <200518@unsaac.edu.pe>
2025-05-18 21:39:55 -0500

    update global variables for nodes, edges, and id

--- src/features/modals/canva/DiagramView.tsx
+++ src/features/modals/canva/DiagramView.tsx
@@ -1,26 +1,33 @@
-import DatabaseDiagram from "./components/DataBaseDiagram";
-import { LayoutDiagram } from "./LayoutDiagram";
-
 import { isRouteErrorResponse } from "react-router";
 import type { Route } from "./+types/DiagramView";
+
 import { transformSolutionModel } from "@/lib/solutionConversion";
+import { useCanvasStore } from "@/state/canvaStore";
+
+import DatabaseDiagram from "./components/DataBaseDiagram";
+import { LayoutDiagram } from "./LayoutDiagram";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const url = `${backendUrl}/solutions/${params.diagramId}`;
+  const { diagramId } = params;
+
+  const url = `${backendUrl}/solutions/${diagramId}`;
 
   if (import.meta.env.MODE !== "production") {
     console.log("URL", url);
   }
-  const response = await fetch(url, {
-    method: "GET",
-  });
+
+  const response = await fetch(url, { method: "GET" });
   const data = await response.json();
+
   if (!response.ok) {
     throw new Error(data.detail);
   }
-  return transformSolutionModel(data);
+
+
+  const transformed = transformSolutionModel(data);
+  return transformed;
 }
 
 // Placeholder for future action functionality. This function is intentionally left empty.
@@ -51,13 +58,18 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
 }
 
 export default function DiagramView({ loaderData }: Route.ComponentProps) {
+  const { setNodes, setEdges, setQueries, setId } = useCanvasStore.getState();
+  
+  setId(loaderData.solutionId);
+  setNodes(loaderData.initialNodes);
+  setEdges(loaderData.initialEdges);
+  setQueries(loaderData.queries);
+  
+
   return (
     <>
       <LayoutDiagram title={loaderData.name}>
-        <DatabaseDiagram
-          initialNodes={loaderData.initialNodes}
-          initialEdges={loaderData.initialEdges}
-        />
+        <DatabaseDiagram />
       </LayoutDiagram>
     </>
   );
