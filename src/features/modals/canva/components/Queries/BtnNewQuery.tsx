ba61bfd9f02121058985e67c2da3c14a239b03b5
Anghelo Alagon <200518@unsaac.edu.pe>
2025-05-25 01:04:55 -0500

    implement modals to support new query creation

--- src/features/modals/canva/components/Queries/BtnNewQuery.tsx
+++ src/features/modals/canva/components/Queries/BtnNewQuery.tsx
@@ -1,12 +1,23 @@
+import { useRef } from "react";
+import { ModalNewQuery } from "./ModalNewQuery";
+
 export const BtnNewQuery = () => {
+	const modalRef = useRef<HTMLDialogElement>(null);
 	return (
-		<button
-			type="button"
-			className="w-full h-auto py-4 flex justify-center rounded-xl items-center border group border-dashed border-lighter-gray bg-transparent text-lighter-gray hover:cursor-pointer hover:border-white hover:text-white"
-		>
-			<div className="py-1.5 px-5 border border-lighter-gray bg-transparent rounded-lg text-h5 group-hover:border-white group-hover:text-whitea ">
-				Agregar Consulta
-			</div>
-		</button>
+		<>
+			<button
+				type="button"
+				onClick={() => {
+					if (modalRef.current) modalRef.current.showModal();
+				}}
+				className="w-full h-auto py-4 flex justify-center rounded-xl items-center border group border-dashed border-lighter-gray bg-transparent text-lighter-gray hover:cursor-pointer hover:border-white hover:text-white"
+			>
+				<div className="py-1.5 px-5 border border-lighter-gray bg-transparent rounded-lg text-h5 group-hover:border-white group-hover:text-whitea ">
+					Agregar Consulta
+				</div>
+			</button>
+
+			<ModalNewQuery modalRef={modalRef} />
+		</>
 	);
 };
