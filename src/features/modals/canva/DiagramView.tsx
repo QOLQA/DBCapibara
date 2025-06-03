c400fb76eb6aaf765300011d7c9b31e8e60ffef1
Anghelo Alagon <93554955+anghelo-code@users.noreply.github.com>
2025-06-02 21:13:03 -0500

    Revert "feat: add atributes to table - generic modal (#58)"
    This reverts commit 46428422be4e03095c104b6145f07622953bdb2b.

--- src/features/modals/canva/DiagramView.tsx
+++ src/features/modals/canva/DiagramView.tsx
@@ -59,8 +59,6 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
 }
 
 export default function DiagramView({ loaderData }: Route.ComponentProps) {
-	// mostrar la ruta actual
-	console.log("loaderData", loaderData);
 	const {
 		id,
 		setNodes,
