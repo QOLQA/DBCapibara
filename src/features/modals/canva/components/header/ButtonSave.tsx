f83d235d5ffe5031af0762a2d93b0023cc41f58b
welpmoz <roswell_pando@outlook.es>
2025-06-13 07:05:19 -0500

    download image from canvas when save version

--- src/features/modals/canva/components/header/ButtonSave.tsx
+++ src/features/modals/canva/components/header/ButtonSave.tsx
@@ -2,6 +2,19 @@ import { Save } from "@/components/icons/HeaderIcons";
 import { transformVersionToBackend } from "@/lib/canvaConversion";
 import { saveCanvas } from "@/lib/saveCanvas";
 import { useCanvasStore } from "@/state/canvaStore";
+import { getNodesBounds, getViewportForBounds } from "@xyflow/react";
+import { toPng, toSvg } from "html-to-image";
+
+const imageWidth = 1024;
+const imageHeight = 768;
+
+function downloadImage(dataUrl: string) {
+  const a = document.createElement('a');
+ 
+  a.setAttribute('download', 'reactflow.png');
+  a.setAttribute('href', dataUrl);
+  a.click();
+}
 
 export const ButtonSave = () => {
 	const Id = useCanvasStore((state) => state.id);
@@ -23,7 +36,26 @@ export const ButtonSave = () => {
 		);
 
 		saveCanvas(Id, versionId, diagram);
+
+		generateImage()
 	};
+
+	const generateImage = () => {
+		const nodesBounds = getNodesBounds(nodes)
+		const viewport = getViewportForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2)
+
+		toSvg(document.querySelector('.react-flow__viewport') as HTMLElement, {
+			backgroundColor: '#171717',
+			width: imageWidth,
+			height: imageHeight,
+			style: {
+				width: imageWidth,
+				height: imageHeight,
+				transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`
+			},
+			skipFonts: true
+		}).then(downloadImage)
+	}
 
 	return (
 		<button type="button" className="group cursor-pointer" onClick={handleSave}>
